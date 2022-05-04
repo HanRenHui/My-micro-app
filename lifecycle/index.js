7653db8828e7e09fed826bbf2a447140f1add904
hanrenhui <hanrenhui@tuhu.cn>
2022-05-04 13:16:01 +0800

    update

--- lifecycle/index.js
+++ lifecycle/index.js
@@ -1,111 +1,113 @@
-import { getList } from './../const/subApp'
-import { getLifyCycle } from './../const/lifeCycle'
+import { getList } from './../const/subApp';
+import { getLifyCycle } from './../const/lifeCycle';
 import { sandBox } from './../sandBox/index';
 
 function findApp(key) {
-    return getList().find(item => {
-        return item.activeRule === key
-    } )
+  return getList().find((item) => {
+    return item.activeRule === key;
+  });
 }
 
 async function runMainLifeCycle(key) {
-    await Promise.all(getLifyCycle()[key].map(async item => await item()))
+  await Promise.all(getLifyCycle()[key].map(async (item) => await item()));
 }
 
 export async function lifeCycle() {
-    const preApp = findApp(window.__ORIGIN_APP__)    
-    const nextApp = findApp(window.__CURRENT_APP__)
-    
-    if (!nextApp) {
-        return;
-    }
-    if(preApp) {
-        await unmount(preApp)
-    }
+  const preApp = findApp(window.__PRE_APP__);
+  const nextApp = findApp(window.__CURRENT_APP__);
 
-    const app = await beforeLoad(nextApp)
+  if (!nextApp) {
+    return;
+  }
+  if (preApp) {
+    await unmount(preApp);
+  }
 
-    await mount(app)
+  const app = await beforeLoad(nextApp);
+
+  await mount(app);
 }
 
 async function unmount(app) {
-    app.proxy.unActive()
-    await app.unmount && app.unmount()
-    await runMainLifeCycle('destroyed')
+  app.proxy.unActive();
+  (await app.unmount) && app.unmount();
+  await runMainLifeCycle('destroyed');
 }
 
-const cache = {}
+const cache = {};
 
 export async function parseHtml(app) {
-    if (cache[app.name]) return cache[app.name]
+  if (cache[app.name]) return cache[app.name];
 
-    const res = await fetch(app.entry)
-    const htmlContent = await res.text()
+  const res = await fetch(app.entry);
+  const htmlContent = await res.text();
 
-    let allScripts = []
+  let allScripts = [];
 
-    const div = document.createElement('div')
-    div.innerHTML = htmlContent
-    const [dom, scriptUrls, scripts] = getResources(div, app)
-    const scriptContents = await Promise.all(scriptUrls.map(async url => {
-        const res = await fetch(url)
-        return await res.text()
-    }))
-    allScripts = [...scripts, ...scriptContents]
+  const div = document.createElement('div');
+  div.innerHTML = htmlContent;
+  const [dom, scriptUrls, scripts] = getResources(div, app);
+  const scriptContents = await Promise.all(
+    scriptUrls.map(async (url) => {
+      const res = await fetch(url);
+      return await res.text();
+    })
+  );
+  allScripts = [...scripts, ...scriptContents];
 
-    cache[app.name] = [dom, allScripts]
+  cache[app.name] = [dom, allScripts];
 
-    return [dom, allScripts]
+  return [dom, allScripts];
 }
 
 async function beforeLoad(app) {
-    runMainLifeCycle('beforeLoad')
+  runMainLifeCycle('beforeLoad');
 
-    const [dom, allScripts] = await parseHtml(app)
+  const [dom, allScripts] = await parseHtml(app);
 
-    // app.beforeLoad && app.beforeLoad()
-    const el = document.querySelector(app.container)
+  // app.beforeLoad && app.beforeLoad()
+  const el = document.querySelector(app.container);
 
-    el.innerHTML = dom;
+  el.innerHTML = dom;
 
-    allScripts.forEach(s => sandBox(s, app))
-    
-    return app
-    // return appContext
+  allScripts.forEach((s) => sandBox(s, app));
+
+  return app;
+  // return appContext
 }
 
 function mount(app) {
-    app.mount && app.mount()
-    runMainLifeCycle('mounted')
+  app.mount && app.mount();
+  runMainLifeCycle('mounted');
 }
 
 function getResources(root, app) {
-    const { entry } = app
-    const scripts = []
-    const scriptUrls = []
-    const deepParse = (ele) => {
-        if (!ele) return;
-        if(ele.tagName.toLowerCase() === 'script') {
-            if (ele.src) {
-                if (ele.src.startsWith('http')) {
-                    scriptUrls.push(ele.src)
-                } else {
-                    scriptUrls.push(`http:${entry}${ele.src}`)
-                }
-            } else {
-                scripts.push(ele.innerHTML)
-            }
-            ele.parentNode && ele.parentNode.replaceChild(document.createComment('哈哈哈已被替换'), ele)
+  const { entry } = app;
+  const scripts = [];
+  const scriptUrls = [];
+  const deepParse = (ele) => {
+    if (!ele) return;
+    if (ele.tagName.toLowerCase() === 'script') {
+      if (ele.src) {
+        if (ele.src.startsWith('http')) {
+          scriptUrls.push(ele.src);
+        } else {
+          scriptUrls.push(`http:${entry}${ele.src}`);
         }
-        if (ele.children) {
-            Array.from(ele.children).forEach(child => deepParse(child))
-        }
+      } else {
+        scripts.push(ele.innerHTML);
+      }
+      ele.parentNode &&
+        ele.parentNode.replaceChild(
+          document.createComment('哈哈哈已被替换'),
+          ele
+        );
     }
-    deepParse(root)
-    const dom = root.outerHTML
-    return [
-        dom, 
-        scriptUrls,
-        scripts
-    ]
+    if (ele.children) {
+      Array.from(ele.children).forEach((child) => deepParse(child));
+    }
+  };
+  deepParse(root);
+  const dom = root.outerHTML;
+  return [dom, scriptUrls, scripts];
 }
--- router/index.js
+++ router/index.js
@@ -1,36 +1,42 @@
-import { lifeCycle } from '../lifecycle/index'
+import { lifeCycle } from '../lifecycle/index';
 
 function patchRouter(originEvent, eventName) {
-    return function() {
-        const e = new Event(eventName)
-        originEvent.apply(this, arguments)
-        window.dispatchEvent(e)
-    }
+  return function () {
+    const e = new Event(eventName);
+    originEvent.apply(this, arguments);
+    window.dispatchEvent(e);
+  };
 }
 
 function shouleTure() {
-    window.__ORIGIN_APP__ = window.__CURRENT_APP__
-    const pathname = window.location.pathname.match(/^\/\w+/)[0]
-    if (pathname === window.__CURRENT_APP__) {
-        return false;
-    }
-    window.__CURRENT_APP__ = pathname
-    return true
+  window.__PRE_APP__ = window.__CURRENT_APP__;
+  const pathname = window.location.pathname.match(/^\/\w+/)[0];
+  if (pathname === window.__CURRENT_APP__) {
+    return false;
+  }
+  window.__CURRENT_APP__ = pathname;
+  return true;
 }
 
 function tureApp() {
-    if (!shouleTure()) {
-        return
-    }
-    console.log('路由跳转了')
-    lifeCycle()
+  if (!shouleTure()) {
+    return;
+  }
+  console.log('路由跳转了');
+  lifeCycle();
 }
 
 export function rewriteRouter() {
-    window.history.pushState = patchRouter(window.history.pushState, 'micro_push')
-    window.history.replaceState = patchRouter(window.history.replaceState, 'micro_replace')
+  window.history.pushState = patchRouter(
+    window.history.pushState,
+    'micro_push'
+  );
+  window.history.replaceState = patchRouter(
+    window.history.replaceState,
+    'micro_replace'
+  );
 
-    window.addEventListener('micro_push', tureApp)
-    window.addEventListener('micro_replace', tureApp)
-    window.addEventListener('popstate', tureApp)
+  window.addEventListener('micro_push', tureApp);
+  window.addEventListener('micro_replace', tureApp);
+  window.addEventListener('popstate', tureApp);
 }
